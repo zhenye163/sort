aa6bfe4fb76b47404ddea20fb2fb12ea73b1221d
zhenye163 <dzy930724@163.com>
2019-01-25 14:30:14 +0800

    添加各排序算法的测试以及性能比较代码

--- src/main/java/com/netopstec/sort/test/SortTest.java
+++ src/main/java/com/netopstec/sort/test/SortTest.java
@@ -99,4 +99,70 @@ public class SortTest {
         ShellSort.sort(nums);
         System.out.println("排序后，数组顺序为：" + Arrays.toString(nums));
     }
+
+
+    /**
+     * 各排序算法的性能测试
+     */
+    @Test
+    public void speedTest() {
+        int[] initIntArrays = IntArrayUtil.initIntArrays(100000,10000);
+        System.out.println("排序前，数组顺序为：" + Arrays.toString(initIntArrays));
+        int[] toBubbleSort = initIntArrays.clone();
+        Long toBubbleSortStart = System.currentTimeMillis();
+        BubbleSort.sort(toBubbleSort);
+        Long toBubbleSortEnd = System.currentTimeMillis();
+        System.out.println("冒泡排序耗时：" + (toBubbleSortEnd - toBubbleSortStart) + "毫秒。");
+//        System.out.println("冒泡排序后，数组顺序为：" + Arrays.toString(toBubbleSort));
+        System.out.println("----------------------------------------------------");
+        int[] toHeapSort = initIntArrays.clone();
+        Long toHeapSortStart = System.currentTimeMillis();
+        HeapSort.sort(toHeapSort);
+        Long toHeapSortEnd = System.currentTimeMillis();
+        System.out.println("堆排序耗时：" + (toHeapSortEnd - toHeapSortStart) + "毫秒。");
+//        System.out.println("堆排序后，数组顺序为：" + Arrays.toString(toHeapSort));
+        System.out.println("----------------------------------------------------");
+        int[] toInsertSort = initIntArrays.clone();
+        Long toInsertSortStart = System.currentTimeMillis();
+        InsertSort.sort(toInsertSort);
+        Long toInsertSortEnd = System.currentTimeMillis();
+        System.out.println("直接插入排序耗时：" + (toInsertSortEnd - toInsertSortStart) + "毫秒。");
+//        System.out.println("直接插入排序后，数组顺序为：" + Arrays.toString(toInsertSort));
+        System.out.println("----------------------------------------------------");
+        int[] toMergeSort = initIntArrays.clone();
+        Long toMergeSortStart = System.currentTimeMillis();
+        MergeSort.sort(toMergeSort);
+        Long toMergeSortEnd = System.currentTimeMillis();
+        System.out.println("归并排序耗时：" + (toMergeSortEnd - toMergeSortStart) + "毫秒。");
+//        System.out.println("归并排序后，数组顺序为：" + Arrays.toString(toMergeSort));
+        System.out.println("----------------------------------------------------");
+        int[] toQuickSort = initIntArrays.clone();
+        Long toQuickSortStart = System.currentTimeMillis();
+        QuickSort.sort(toQuickSort);
+        Long toQuickSortEnd = System.currentTimeMillis();
+        System.out.println("快速排序耗时：" + (toQuickSortEnd - toQuickSortStart) + "毫秒。");
+//        System.out.println("快速排序后，数组顺序为：" + Arrays.toString(toQuickSort));
+        System.out.println("----------------------------------------------------");
+        int[] toRadixSort = initIntArrays.clone();
+        Long toRadixSortStart = System.currentTimeMillis();
+        RadixSort.sort(toRadixSort);
+        Long toRadixSortEnd = System.currentTimeMillis();
+        System.out.println("基数排序耗时：" + (toRadixSortEnd - toRadixSortStart) + "毫秒。");
+//        System.out.println("基数排序后，数组顺序为：" + Arrays.toString(toRadixSort));
+        System.out.println("----------------------------------------------------");
+        int[] toSelectSort = initIntArrays.clone();
+        Long toSelectSortStart = System.currentTimeMillis();
+        SelectSort.sort(toSelectSort);
+        Long toSelectSortEnd = System.currentTimeMillis();
+        System.out.println("简单选择排序耗时：" + (toSelectSortEnd - toSelectSortStart) + "毫秒。");
+//        System.out.println("简单选择排序后，数组顺序为：" + Arrays.toString(toSelectSort));
+        System.out.println("----------------------------------------------------");
+        int[] toShellSort = initIntArrays.clone();
+        Long toShellSortStart = System.currentTimeMillis();
+        ShellSort.sort(toShellSort);
+        Long toShellSortEnd = System.currentTimeMillis();
+        System.out.println("希尔排序耗时：" + (toShellSortEnd - toShellSortStart) + "毫秒。");
+//        System.out.println("希尔排序后，数组顺序为：" + Arrays.toString(toShellSort));
+    }
+
 }
